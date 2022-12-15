6e922dc8f04700c5badfd509a556082c8d05da04
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2022-12-16 00:03:05 +0530

    Day 18 Stuffs ~ While Loops

---
+++ Day 18/demo.py
@@ -0,0 +1,13 @@
+# i=int(input("Enter A Number"))
+count=0
+while count<5:
+    print(count)
+    count+=1
+
+#while-else
+count1 = 5
+while (count1 > 0):
+  print(count1)
+  count1 = count1 - 1
+else:
+  print("I am inside else")
