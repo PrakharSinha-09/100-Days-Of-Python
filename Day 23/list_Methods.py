da949a76f6ecb4ed38f2cddd2ba26946cc205d49
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2022-12-21 00:24:25 +0530

    Day 23 Stuffs ~ Lists Methods(Some Changes)

--- Day 23/list_Methods.py
+++ Day 23/list_Methods.py
@@ -18,6 +18,26 @@ print(listt.index(1))
 #count()
 print(listt.count(2))
 
-m=listt
+# m=listt        Avoid Copying like this
+# m[0]=3
+# print(listt)        #yes original list changed, because line 21, conveys that the reference of listt is passed to m, so any changes made via m will get reflected in the original one too!
+                    #so, it is better to use .copy() method to copy the list
+
+#copy()
+m=listt.copy()
 m[0]=3
-print(listt)        #yes original list changed, because line 21, conveys that the reference of listt
+print(listt)      #this time, no change
+
+#insert() method helps to insert the item at the provided index....and this list will increase by one
+listt.insert(2,"Hey Prakhar")
+print(listt) 
+
+#extend() method adds an entire list or any other collection datatype(set,tuple,dictionary) to the existing list @ the end
+m=[21,31]
+listt.extend(m)
+print(listt)   #this changes originial list "listt" right!...But What if, we want to concatenate, but keeping all these the seperate entity...
+                #but it should be of same type only, i.e., it must be list too
+
+#Using + operator to concatenate the list
+k=listt+m
+print(k)
