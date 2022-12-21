fc6f0064fdc764b1a89446e04dfbb3f26afac346
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2022-12-21 19:46:47 +0530

    Day 24 Stuffs ~ Tuples Intro

---
+++ Day 24/demo.py
@@ -0,0 +1,11 @@
+tuple1 = (1,2,2,3,5,4,6)
+tuple2 = ("Red", "Green", "Blue")
+print(tuple1)
+print(tuple2)
+
+print(tuple1[2])                        #accessing tuple through index.
+#Just Like in lists, we did slicing, it is possible here too!
+
+print(tuple1[2:5])
+
+#And Refe
