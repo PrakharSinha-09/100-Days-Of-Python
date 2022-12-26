6376facf864de6addb698498ac424d1910a7a983
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2022-12-26 22:01:50 +0530

    Day 29 Stuffs ~ DocStrings & PEP8

---
+++ Day 29/demo.py
@@ -0,0 +1,5 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''      #this is a docstring
+    print(n**2)
+square(5)
+print(square.__doc__)               #to print the docstring
