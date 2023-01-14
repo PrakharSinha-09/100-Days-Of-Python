4d520c939e2b5895784b2bf6aef3a3225d7792ba
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-01-14 23:50:23 +0530

    Day 48 Stuffs ~ Local & Global Variables

---
+++ Day 48/demo.py
@@ -0,0 +1,10 @@
+x=9
+print(x)
+
+def fun():
+    global x                                       #you can directly initialize like global x=8, this will be error..This Simple means catch the global variable x via preceding it via global keyboard
+    x=8                                            #Updating
+    print(f"Local Value oF x is {x}")              
+
+fun()
+print(f"The Global Value of x is {x}")             #Global Variable will also change to 8
